c349502f1df923edaf810cb575ad782845b354a6
oussamaamou <amoussdark@gmail.com>
2025-01-05 23:48:48 +0100

    Gestion des articles

---
+++ views/manipuler_articles.php
@@ -0,0 +1,176 @@
+<?php
+
+require '../classes/Connexion.php';
+require '../classes/Utilisateur.php';
+require '../classes/Article.php';
+
+session_start();
+
+if(!isset($_SESSION['ID'])){
+    header('location: ../public/login.php');
+    exit();
+}
+
+$ID = $_SESSION['ID'];
+
+$db = new DataBase();
+$conn = $db->getConnection();
+
+$auteur = new Utilisateur($conn);
+$profile = $auteur->getProfileInfos($ID);
+
+$article = new Article($conn);
+$posters = $article->getStatutArticles();
+
+if ($profile) {
+    $nom = $profile['Nom'];
+    $prenom = $profile['Prenom'];
+    $photo = $profile['Photo'];
+    $role = $profile['Role'];
+    $telephone = $profile['Telephone'];
+    $email = $profile['Email'];
+} else {
+
+    echo "Profile non trouvé.";
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['statut']) && isset($_POST['article_id'])) {
+        $statut = $_POST['statut'];
+        $article_id = $_POST['article_id'];
+
+        $updateSuccess = $article->updateArticleStatut($article_id, $statut);
+
+        header("Refresh: 1");
+        exit();
+    }
+}
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Lien du Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Lien des Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+    
+    <title>Postes</title>
+</head>
+<body class="flex bg-gray-100 min-h-screen">
+    <aside class="hidden sm:flex sm:flex-col">
+        <a href="#" class="inline-flex items-center justify-center h-20 w-20 hover:bg-purple-500 focus:bg-purple-500">
+            <img src="../assets/images/Shared_Horizons_Logo.png" alt="Site Web Logo" />
+        </a>
+        <div class="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
+        <nav class="flex flex-col mx-4 my-6 space-y-4">
+            
+            <a href="gestion.php" class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+            <span class="sr-only">Dashboard</span>
+            <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+            </svg>
+            </a>
+
+            <a href="manipuler_articles.php" class="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
+            <span class="sr-only">Messages</span>
+            <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            </a>
+
+            <a href="profile_administrateur.php" class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+            <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            </a>
+            
+        </nav>
+        </div>
+    </aside>
+    <div class="flex-grow text-gray-800">
+        <header class="flex items-center h-20 px-6 sm:px-10 bg-white">
+            <div class="flex flex-shrink-0 items-center ml-auto">
+                <div class="hidden md:flex md:flex-col md:items-end md:leading-tight">
+                    <span class="font-semibold"><?php echo $nom . ' ' . $prenom; ?></span>
+                    <span class="text-sm text-gray-600"><?php echo $role; ?></span>
+                </div>
+                <span class="h-12 w-12 ml-2 sm:ml-3 mr-2 bg-gray-100 rounded-full overflow-hidden">
+                    <img src="uploads/<?php echo $photo; ?>" alt="user profile photo" class="h-full w-full object-cover">
+                </span>
+            
+                <div class="border-l pl-3 ml-3 space-x-1">
+                    <button class="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
+                    <a href="../public/logout.php">
+                        <span class="sr-only">Log out</span>
+                        <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                    </a>
+                    </button>
+                </div>
+            </div>
+        </header>
+
+
+        <main>
+
+            <section>
+                
+                <!-- Post Card-->
+                <?php foreach($posters as $poster) { ?>
+                <div class="mt-[4rem] ml-[20rem] bg-white dark:bg-gray-800 rounded-xl shadow-2xl max-w-4xl w-full p-8 transition-all duration-300 animate-fade-in pt-[3.5rem]">
+                    <div class="flex flex-col md:flex-row">
+                        <div class="md:w-1/3 text-center mb-8 md:mb-0">
+                            <img src="uploads/<?php echo $poster['auteur_photo']; ?>" alt="Profile Picture" class="rounded-full w-48 h-48 mx-auto mb-4 border-4 border-purple-500 transition-transform duration-300 hover:scale-105">
+                            <h1 class="text-2xl font-bold text-purple-500 dark:text-white mb-2"></h1>
+                            <p class="text-stone-700 font-semibold"><?php echo htmlspecialchars($poster['auteur_nom']) . ' ' . htmlspecialchars($poster['auteur_prenom']); ?></p>
+                            
+
+                            <h2 class="text-xl font-semibold text-purple-500 mb-4  mt-[3rem]">Catégorie</h2>
+                            <p class="text-stone-700 font-semibold"><?php echo htmlspecialchars($poster['categorie_nom']) ?></p>
+
+                            <form class="mt-[8rem]" action="manipuler_articles.php" method="POST">
+                                <input type="hidden" name="article_id" value="<?php echo $poster['ID']; ?>">
+                                <button type="submit" name="statut" value="Accepté" 
+                                class="mr-[2rem] text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Accepter</button>
+                                <button type="submit" name="statut" value="Refusé" 
+                                class="text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Refuser</button>
+                            </form>
+                            
+                        </div>
+                        <div class="md:w-2/3 md:pl-8">
+                            <p class="text-gray-700 font-semibold text-2xl dark:text-gray-300 mb-6"> <?php echo htmlspecialchars($poster['Titre']) ?></p>
+                            <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
+                                <img
+                                    class="object-cover object-center w-full h-96"
+                                    src="https://images.pexels.com/photos/4050347/pexels-photo-4050347.jpeg?cs=srgb&dl=pexels-vlada-karpovich-4050347.jpg&fm=jpg"
+                                    alt="nature image"
+                                />
+                            </div>
+                            <p class="text-gray-700 dark:text-gray-300 mb-6 mt-6">
+                                <?php echo htmlspecialchars($poster['Contenu']) ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+                
+            </section>
+
+        </main>
+    </div>
+
+</body>
+</html>
