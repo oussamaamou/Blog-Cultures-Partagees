bbb197c3c6499c4b33723089165b1e042e109a56
oussamaamou <amoussdark@gmail.com>
2025-01-10 22:16:34 +0100

    Modifier la classe Categorie

--- classes/Categorie.php
+++ classes/Categorie.php
@@ -3,18 +3,36 @@
 class Categorie{
 
     private $conn;
+    private $nom;
+    private $description;
 
     public function __construct($conn){
         $this->conn = $conn;
     }
 
-    public function creerCategorie($nom, $description){
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function setDescription($description){
+        $this->description = $description;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function creerCategorie(){
 
         $sql = ("INSERT INTO categorie (Nom, Description) VALUES (:nom, :description)");
 
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
-        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+        $stmt->bindParam(':nom', $this->nom, PDO::PARAM_STR);
+        $stmt->bindParam(':description', $this->description, PDO::PARAM_STR);
 
         return $stmt->execute();
     }
@@ -33,6 +51,51 @@ class Categorie{
         return [];
        }
         
+    }
+
+    public function creerTag(){
+
+        $sql = ("INSERT INTO tags (Nom) VALUES (:nom)");
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':nom', $this->nom, PDO::PARAM_STR);
+
+        return $stmt->execute();
+    }
+
+    public function getAllTags(){
+        try{ 
+            $sql = ("SELECT * FROM tags");
+            $stmt = $this->conn->prepare($sql);
+            
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+       }
+       catch(PDOException $e){
+        echo "Error: " . $e->getMessage();
+        return [];
+       }
+        
+    }
+
+    public function deleteCategorie($id){
+
+        $stmt = $this->conn->prepare("DELETE FROM categorie WHERE ID = :id");
+
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        
+    
+        return $stmt->execute();
+    }
+
+    public function deleteTags($id){
+
+        $stmt = $this->conn->prepare("DELETE FROM tags WHERE ID = :id");
+
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        
+    
+        return $stmt->execute();
     }
 
 
