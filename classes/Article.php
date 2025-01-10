1478e9ad0866a7481791f1bc8227330140d505b6
oussamaamou <amoussdark@gmail.com>
2025-01-10 22:16:03 +0100

    Modifier la classe Article

--- classes/Article.php
+++ classes/Article.php
@@ -18,28 +18,80 @@ class Article {
         $this->conn = $conn;
     }
 
-    public function setArticleData($ID_auteur, $ID_categorie, $titre, $contenu, $image, $statut, $date_publication) {
+    public function setAuteur($ID_auteur) {
         $this->ID_auteur = $ID_auteur;
+    }
+
+    public function setCategorie($ID_categorie) {
         $this->ID_categorie = $ID_categorie;
+    }
+
+    public function setTitre($titre) {
         $this->titre = $titre;
+    }
+
+    public function setContenu($contenu) {
         $this->contenu = $contenu;
+    }
+
+    public function setImage($image) {
         $this->image = $image;
+    }
+
+    public function setStatut($statut) {
         $this->statut = $statut;
+    }
+
+    public function setDatePublication($date_publication) {
         $this->date_publication = $date_publication;
     }
+
+
+    public function getAuteur() {
+        return $this->ID_auteur;
+    }
+
+    public function getCategorie() {
+        return $this->ID_categorie;
+    }
+
+    public function getTitre() {
+        return $this->titre;
+    }
+
+    public function getContenu() {
+        return $this->contenu;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function getStatut() {
+        return $this->statut;
+    }
+
+    public function getDatePublication() {
+        return $this->date_publication;
+    }
+
+
+
+
 
     public function addArticle() {
         $sql = "INSERT INTO article (ID_auteur, ID_categorie, Titre, Contenu, Image, Statut, Date_publication)
                 VALUES (:ID_auteur, :ID_categorie, :titre, :contenu, :image, :statut, :date_publication)";
+        
         $stmt = $this->conn->prepare($sql);
 
         $stmt->bindParam(':ID_auteur', $this->ID_auteur, PDO::PARAM_INT);
         $stmt->bindParam(':ID_categorie', $this->ID_categorie, PDO::PARAM_INT);
         $stmt->bindParam(':titre', $this->titre, PDO::PARAM_STR);
         $stmt->bindParam(':contenu', $this->contenu, PDO::PARAM_STR);
-        $stmt->bindParam(':image', $this->image, PDO::PARAM_STR); 
         $stmt->bindParam(':statut', $this->statut, PDO::PARAM_STR);
         $stmt->bindParam(':date_publication', $this->date_publication, PDO::PARAM_STR);
+        $stmt->bindParam(':image', $this->image, PDO::PARAM_LOB);
 
         return $stmt->execute();
     }
@@ -102,7 +154,6 @@ class Article {
                 WHERE article.Statut = :statut"
             );
             
-            // Utilisation de la propriété statutAccepté
             $stmt->bindParam(':statut', $this->statutAccepté, PDO::PARAM_STR);
     
             $stmt->execute();
@@ -130,7 +181,6 @@ class Article {
                 WHERE article.Statut = :statut"
             );
             
-            // Utilisation de la propriété statutEnCours
             $stmt->bindParam(':statut', $this->statutEnCours, PDO::PARAM_STR);
     
             $stmt->execute();
@@ -158,6 +208,40 @@ class Article {
             return false;
         }
     }
+
+    public function updateArticle($ID_article) {
+        try {
+            $sql = "UPDATE article SET Titre = :titre, Contenu = :contenu, ID_categorie = :ID_categorie, Image = :image, Statut = :statut, Date_publication = :date_publication WHERE ID = :ID_article";
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->bindParam(':ID_article', $ID_article, PDO::PARAM_INT);
+            $stmt->bindParam(':titre', $this->titre, PDO::PARAM_STR);
+            $stmt->bindParam(':contenu', $this->contenu, PDO::PARAM_STR);
+            $stmt->bindParam(':ID_categorie', $this->ID_categorie, PDO::PARAM_INT);
+            $stmt->bindParam(':image', $this->image, PDO::PARAM_LOB);
+            $stmt->bindParam(':statut', $this->statut, PDO::PARAM_STR);
+            $stmt->bindParam(':date_publication', $this->date_publication, PDO::PARAM_STR);
+
+            return $stmt->execute();
+            
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function deleteArticle($id){
+
+        $stmt = $this->conn->prepare("DELETE FROM article WHERE ID = :id");
+
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        
+    
+        return $stmt->execute();
+
+    }
+    
 }
 
 ?>
