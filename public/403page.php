49a380bc1c5bc91b7d565034eac138c0e1cd4d2b
oussamaamou <amoussdark@gmail.com>
2025-01-10 22:25:26 +0100

    Regler la page 403

---
+++ public/403page.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Lien du Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Lien des Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+    
+    <title>Shared Horizons</title>
+</head>
+<body class="bg-purple-300">
+
+    
+
+    <main class="pt-[5rem] pb-[5rem]">
+
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div class="mx-auto max-w-screen-sm text-center">
+                    <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">403</h1>
+                    <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Access Forbidden</p>
+                    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, it's not allowed to go beyond this point!</p>
+                    <a href="index.php" class="inline-flex text-white bg-purple-600 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</a>
+                </div>   
+            </div>
+        </section>
+       
+    </main>
+
+    
+
+    
+
+
+</body>
+</html>
